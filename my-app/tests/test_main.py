cacff741c0801de86d89081de72fc8d4fc9ff9a9
TriniUSAL <trinidadburs@usal.es>
2024-11-08 10:02:45 +0100

    fix

--- my-app/tests/test_main.py
+++ my-app/tests/test_main.py
@@ -3,15 +3,13 @@ import pytest
 from fastapi.testclient import TestClient
 
 
-class TestGenericFunctionaliti(TestCase): 
+class TestGenericFunctionaliti(TestCase):
 
     @pytest.mark.asyncio
     @mock.patch("app.main.initialize", new_callable=mock.AsyncMock)
     @mock.patch("app.main.on_finish", new_callable=mock.AsyncMock)
     async def test_lifespan(self, mk_on_finish, mk_initialize):
-        """
-        Test that `initialize` and `on_finish` are called correctly within the lifespan context manager.
-        """
+        """ Test that `initialize` and `on_finish` are called correctly within the lifespan context manager. """
         from app.main import app   # It's imported iniside so the original "initialize" and "on_finish" aren't called/imported
 
         with TestClient(app) as client:
